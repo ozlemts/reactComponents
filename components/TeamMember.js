59c3bf7488ad72bccb4bdeaefb7aff09acffd17d
OzlemTS <ozlemturkerii@gmail.com>
2021-02-09 18:47:24 +0300

    team member has added

--- components/TeamMember.js
+++ components/TeamMember.js
@@ -3,17 +3,19 @@ import React, { useState } from "react";
 export const TeamMember = (props) => {
 
 	return (
-		<div className="w-full d-flex justify-content-between align-items-center mb-1">
-			<div className="d-flex align-items-center">
-				<img className="h-3 mr-2" src={props.img_src}/>
-				<div>
-					<div>{props.name}</div>
-					<div className="text-detail">{props.status}</div>
+		<div>
+			<div className="w-full d-flex justify-content-between align-items-center p-1 member-container mb-05">
+				<div className="d-flex align-items-center">
+					<img className="h-3 mr-2" src={props.img_src}/>
+					<div>
+						<div className="mb-05">{props.name}</div>
+						<div className="text-detail">{props.status}</div>
+					</div>
 				</div>
+				<span className="text-detail" style={
+					{fontSize: '1.2rem'}
+				}>{props.share} %</span>
 			</div>
-			<span className="text-detail" style={
-				{fontSize: '1.2rem'}
-			}>{props.share} %</span>
 		</div>
 	);
 };
